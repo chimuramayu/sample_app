3988f12e626282a1d0648742146aac484290c381
chimuramayu <chimuramayu@gmail.com>
2019-11-26 15:12:20 +0900

    [finish] 3-9

---
+++ app/controllers/todolists_controller.rb
@@ -0,0 +1,45 @@
+class TodolistsController < ApplicationController
+  def new
+  	# Viewへ渡すためのインスタンス変数に空のモデルオブジェクトを作成する
+  	@list = List.new
+  end
+
+  def create
+    # ストロングパラメーターを追加
+    list = List.new(list_params)
+    # DBへ保存する
+    list.save
+    # トップ画面へリダイレクト
+    redirect_to todolist_path(list.id)
+  end
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+  	@list = List.find(params[:id])
+  end
+
+  def edit
+  	@list = List.find(params[:id])
+  end
+
+  def update
+  	list = List.find(params[:id])
+  	list.update(list_params)
+  	redirect_to todolist_path(list.id)
+  end
+
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to todolists_path
+  end
+
+  private
+
+  def list_params
+  	params.require(:list).permit(:title, :body, :image)
+  end
+end
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '80242bc1dccdd4ab8c25d0a9ca6ae5c956213151d9003443e30a7f602cc6475442e80eaf8257838923571541a50b89f37fdb871366371498e82a586ee87e7c0f'
